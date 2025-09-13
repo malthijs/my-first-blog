0797d0c3be1d7fe5fd782db87b02513edfe3d392
matthijs <matthijs-de-vries@hotmail.com>
2025-09-13 14:30:07 +0200

    new data

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to my homepage!")
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
-
+from blog import views  # <-- import your views here
 urlpatterns = [
     path("admin/", admin.site.urls),
     *debug_toolbar_urls(),
+    path('', views.home, name='home'),
 ]
